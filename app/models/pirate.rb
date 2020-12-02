3c3dff2dd097f792c196940be5963496ac22c36a
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2020-12-02 13:17:06 -0500

    commit tree 50a19fa05035c6e08d474cd8cba71d4afc6de30e

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,19 @@
 class Pirate
+
+    attr_accessor :name, :weight, :height
+
+    @@all = []
+
+    def initialize(args)
+        @name = args[:name]
+        @weight = args[:weight]
+        @height = args[:height]
+        @ships = []
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
 end
